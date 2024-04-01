be81d66950b1ab05fdb9e1604479967ce8cbd791
arunkumark98 <104649871+arunkumark98@users.noreply.github.com>
2024-04-01 18:45:12 +0530

    Initial commit

---
+++ src/Mmfs.js
@@ -0,0 +1,14 @@
+function Mmfs() {
+  return (
+    <div>
+      <h1>Mobile Manufacturers</h1>
+      <ul>
+        <li>Samsung</li>
+        <li>HTC</li>
+        <li>Micromax</li>
+        <li style={{ listStyleType: "circle" }}>Apple</li>
+      </ul>
+    </div>
+  );
+}
+export default Mmfs;
---
+++ src/Mos.js
@@ -0,0 +1,14 @@
+function Mos() {
+  return (
+    <div>
+      <h1>Mobile Operating System</h1>
+      <ul>
+        <li>Android</li>
+        <li>Blckberry</li>
+        <li>iPhone</li>
+        <li>Windows Phone</li>
+      </ul>
+    </div>
+  );
+}
+export default Mos;
